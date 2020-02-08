39ed148a81a84271f9152c1a7c23a7d65eeae4f1
Tyler Coleman <tyler.coleman@gmail.com>
2020-02-08 15:15:50 -0800

    Set up framework for item script
    this will be a script used to track the name type and value of all items in the game

---
+++ SmallBusinessGame/Assets/Scripts/General Scripts/ItemScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    //Private Variables
+    private string itemType;
+    private string itemName;
+    private string itemValue;
+    //Public Variables
+    public string ItemType{
+        get { return itemType; }
+        set { }
+    }
+    public string ItemName {
+        get { return itemName; }
+        set { }
+    }
+    public string ItemValue
+    {
+        get { return ItemValue; }
+        set { }
+    }
+}
