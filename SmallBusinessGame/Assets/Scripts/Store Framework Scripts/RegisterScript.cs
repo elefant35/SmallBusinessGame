f9ab020fc3d4ebccef3b2123929aef50eee6c936
Tyler Coleman <tyler.coleman@gmail.com>
2020-02-08 15:04:36 -0800

    Initial Commit

---
+++ SmallBusinessGame/Assets/Scripts/Store Framework Scripts/RegisterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterScript : MonoBehaviour
+{
+    //Private Variables
+    private float moneyInRegister;
+
+    //Public Variables
+    public float MoneyInRegister //Tracks the Money currently in the register
+    {
+        get
+        {
+            return moneyInRegister;
+        }
+        set
+        {
+            moneyInRegister = value;
+        }
+    }
+
+
+
+
+}
