856799e3e823af25c6614b1585aa76fe88964077
Tyler Coleman <tyler.coleman@gmail.com>
2020-02-16 16:14:13 -0800

    Added Stations and walking animations

--- SmallBusinessGame/Assets/Scripts/NPC Scripts/WalkController.cs
+++ SmallBusinessGame/Assets/Scripts/NPC Scripts/WalkController.cs
@@ -6,7 +6,10 @@ using UnityEngine.AI;
 public class WalkController : MonoBehaviour
 {
     [SerializeField] private Transform goal; // goal is the location where the character should go
-
+    [SerializeField] private Vector3 previousPosition;
+    [SerializeField] private float velocity;
+    [SerializeField] private float velocityConst;
+    [SerializeField] private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +19,24 @@ public class WalkController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //calculates and sends velocity information to the animator
+        velocity = Mathf.Sqrt(Mathf.Pow((transform.position.z - previousPosition.z ),2)+ Mathf.Pow((transform.position.x - previousPosition.x),2)) / Time.deltaTime;
+        previousPosition = transform.position;
+        animator.SetFloat("Speed_f", velocity / velocityConst);
     }
 
     //Public functions
     public void ChangeGoal(Transform newGoal)
     {
-        goal = newGoal;
+        if(newGoal.GetComponentInChildren<StationScript>() != null)
+        {
+            goal = newGoal.GetComponentInChildren<StationScript>().gameObject.transform;
+        }
+        else
+        {
+            goal = newGoal;
+        }
+
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
     }
